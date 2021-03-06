6ebf6fb1346c689f0d6d46ba4ca843c2a45cc56e
laura <laura@LAPTOP-5OQ997SP>
2021-03-05 21:31:38 -0300

    Início da página de meditação

---
+++ index.php
@@ -0,0 +1,89 @@
+
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Ilumini-se</title>
+        <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="site/plugins/fontawesome-free/css/all.min.css">
+        <!-- Theme style -->
+        <link rel="stylesheet" href="site/dist/css/adminlte.min.css">
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="site/css/bootstrap.min.css">
+
+        <link rel="stylesheet" href="site/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"/>
+
+        <!-- overlayScrollbars -->
+        <link rel="stylesheet" href="painel/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+        <!-- Daterange picker -->
+        <link rel="stylesheet" href="painel/plugins/daterangepicker/daterangepicker.css">
+        <!-- summernote -->
+        <link rel="stylesheet" href="painel/plugins/summernote/summernote-bs4.min.css">
+        <!-- DataTables -->
+        <link rel="stylesheet" href="painel/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">       
+        <link rel="stylesheet" href="painel/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+        <link rel="stylesheet" href="painel/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+        <!--link css-->
+
+
+        <!--inico nav-->
+    <ul class="nav  alert alert-warning">
+        <img src="site/dist/img/iluminese (2).png" alt="imagem" class="right mx-auto d-block"/>
+        <li class="nav-item">
+            <a class="nav-link text-danger" href="#">Início</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-danger" href="#">Sonbre</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-danger" href="#">Contato</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-danger" href="#">Podcast</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link text-danger" href="#">Outras páginas</a>
+        </li>
+    </ul>
+
+
+</head>
+<body>
+
+    <!--inicio de container-->
+    <div class="container">
+        <!--inicio de row 1-->
+        <div class="row">
+
+            <!--inicio de coll 2-->
+            <div class="col">
+
+
+            </div> 
+            <!--fim de coll 2-->
+
+            <!--fim de row 1-->
+        </div>
+
+
+    </div>
+
+
+</body>
+<!--inicio da footer-->
+<footer class="balert alert-danger m-auto p-5">
+
+    <div class="footer"> 
+
+        <div class="container">
+            <div class="row">
+
+                <p> <small> ©Direitos reservados a LVADeveloper  </small> </p>
+
+            </div>
+        </div>
+    </div>
+</footer>
+
+</html>
